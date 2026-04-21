0172a9cf4121f1bac419cb6c2afc4bbd099bb52f
YangRongjun <yangrongjun0407@qq.com>
2026-04-21 23:42:51 +0800

    feat: 🚀 首页背景图调整

--- blog-view/src/settings.js
+++ blog-view/src/settings.js
@@ -3,9 +3,9 @@ module.exports = {
 	 * @type {string}
 	 * @description 首页三张背景图
 	 */
-	bg1: 'http://111.228.14.226:19000/kcbjt/blog/bg/bg1.jpg',
-	bg2: 'http://111.228.14.226:19000/kcbjt/blog/bg/bg2.jpg',
-	bg3: 'http://111.228.14.226:19000/kcbjt/blog/bg/bg3.jpg',
+	bg1: '/img/bg/bg1.jpg',
+	bg2: '/img/bg/bg2.jpg',
+	bg3: '/img/bg/bg3.jpg',
 
 	/**
 	 * @type {string}
