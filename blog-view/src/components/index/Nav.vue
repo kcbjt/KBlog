e6d7cbe2e8eaf4399e5943ab2c35a73ffd6de9c8
YangRongjun <yangrongjun0407@qq.com>
2026-04-21 21:54:48 +0800

    feat: 🚀 导航栏新增主站跳转

--- blog-view/src/components/index/Nav.vue
+++ blog-view/src/components/index/Nav.vue
@@ -47,7 +47,7 @@
 					</svg> 
 				</span>
 		    </a>
-			<a href="https://github.com/kcbjt/KBlog"
+			<a href="https://www.kcbjt.com"
 				target="_blank" rel="noopener noreferrer" class="item" :class="{'m-mobile-hide': mobileHide,'active':$route.name==='about'}">主站
 				<span>
 					<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" x="0px" y="0px" 
